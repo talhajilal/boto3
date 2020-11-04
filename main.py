0e80b873dbb11d4f95c23ebcbe23af85bca8471b
talhajilal@gmail.com <talhajilal@gmail.com>
2020-11-04 17:19:48 +0000

    Newchange

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import boto3
-s3 = boto3.resource('ec2')
-print (s3)
+import boto3    
+ec2client = boto3.client('ecs')
+response = ec2client.describe_instances()
+for reservation in response["Reservations"]:
+    for instance in reservation["Instances"]:
+        # This sample print will output entire Dictionary object
+        print(instance)
+        # This will print will output the value of the Dictionary key 'InstanceId'
+        print(instance["InstanceId"])
